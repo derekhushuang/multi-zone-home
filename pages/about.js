f5761bf3349c00a21d35f5af4120657d39f4844e
derekhus <derekhu@aaxischina.com>
2020-04-20 18:26:24 +0800

    update...

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,22 @@
 import React from 'react'
+// 5. shared components and shared function will be saved in shared repository
 import {Footer, Header, requestHelper} from "@aaxis/share-components";
 import _ from 'lodash'
+
+const wcsResourceConfig = { footerResourceId: '1393828675178'}
 
 export default class About extends React.Component{
 
     static async getInitialProps(context) {
+        // 1: layout call
 
+        // 2: wcs call (async call)
+        //Q1: read layout response and then call wcs resource
         const footerReq = requestHelper.fetch({
-            uri: 'https://aaxisproducttest.azureedge.net//public/html/1393828675178.html',
+            uri: `https://aaxisproducttest.azureedge.net//public/html/${wcsResourceConfig.footerResourceId}.html`,
             method: 'get',
         });
+        // 3: api call
         const locationDetailReq = requestHelper.fetch({
             uri: 'https://headless-api.aaxisaws.com/api/location/branches/aurora-co-showroom',
             method: 'get',
@@ -20,15 +27,19 @@ export default class About extends React.Component{
         }catch (e) {
            console.log('request error:', e)
         }
-
+        // 4. if we need read multi wcs resources, we will save the wcs resource with id, and get the resource by id.
+        // for banner wcs content we can make them lazy loading
         return {
-            htmlContent: _.get(result, ['0','data'], ''),
+            wcsResources : {
+                [wcsResourceConfig.footerResourceId] : _.get(result, ['0','data'], '')
+            },
             locationDetail: _.get(result, ['1', 'data'], {})
         };
     }
 
     render() {
         const aboutUs = _.get(this.props.locationDetail, ['location', 'originalLocationDetail', 'currentLocationBusiness', 'aboutUs'], {})
+        const footerContent = _.get(this.props.wcsResources, [wcsResourceConfig.footerResourceId], '');
         return (
             <div>
                 <Header />
@@ -36,7 +47,7 @@ export default class About extends React.Component{
                 <p><b>About Us Title:</b> {aboutUs.title}</p>
                 <p><b>Description:</b> {aboutUs.description}</p>
                 <div><a href='/'>Go Back</a></div>
-                <Footer content={this.props.htmlContent}/>
+                <Footer content={footerContent}/>
             </div>
         )
     }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import Title from "../components/Title";
 import { Header,Footer, requestHelper } from "@aaxis/share-components";
+import _ from "lodash";
+import asset from "next/asset";
 
+const wcsResourceConfig = { footerResourceId: '1393828675178'}
 
 export default class Home extends React.Component{
     static async getInitialProps(context) {
 
         const result = await requestHelper.fetch({
-            uri: 'https://aaxisproducttest.azureedge.net//public/html/1393828675178.html',
+            uri: `https://aaxisproducttest.azureedge.net//public/html/${wcsResourceConfig.footerResourceId}.html`,
             method: 'get',
         })
         return {
-            htmlContent: result && result.data
+            wcsResources : {
+                [wcsResourceConfig.footerResourceId] : _.get(result, 'data', '')
+            }
         };
     }
 
     render() {
+        const footerContent = _.get(this.props.wcsResources, [wcsResourceConfig.footerResourceId], '');
         return (
             <div>
                 <Header />
@@ -26,7 +32,8 @@ export default class Home extends React.Component{
                 <div>
                     <a href="/about">About us</a>
                 </div>
-                <Footer content={this.props.htmlContent}/>
+                <img width={200} src={asset("/nextjs.png")} />
+                <Footer content={footerContent}/>
             </div>
         )
     }
